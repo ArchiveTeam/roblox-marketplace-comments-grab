abf8dd9e3fe3cf098dd62eda0e9d858c9e31de7f
arkiver <arkiver@protonmail.com>
2024-04-04 22:14:51 +0200

    Version 20240405.02. Fix queuing 'asset' item and not 'item' item.

--- roblox-marketplace-comments.lua
+++ roblox-marketplace-comments.lua
@@ -370,7 +370,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     if string.match(url, "^https?://catalog%.roblox%.com/v2/recommendations/assets%?") then
       json = cjson.decode(html)
       for _, new_id in pairs(json["data"]) do
-        discover_item(discovered_items, "item:" .. tostring(new_id))
+        discover_item(discovered_items, "asset:" .. tostring(new_id))
       end
     end
     if string.match(url, "^https?://www%.roblox%.com/comments/get%-json%?") then
